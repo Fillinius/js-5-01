c4a97f07db02601c8d115b57bb97e512f2be82e4
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-10 15:33:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const myName = 'Vladimir'
+
+const programmingLanguage = 'JavaScript'
+
+const courseCreatorName = 'Vladilen'
+
+const reasonText = 'I  am want work in IT'
+
+const numberOfMounth = '2'
+
+const allInfo = `Всем привет! Меня зовут ${myName} Сейчас я изучаю язык программирования ${programmingLanguage}  на курсе по ${programmingLanguage} У ${courseCreatorName} . Я хочу стать веб-разработчиком, потому что ${reasonText} . До этого я изучал ${programmingLanguage} ${numberOfMounth} месяца. Я уверена, что пройду данный курс до конца!"`
+console.log(allInfo)
+
+// console.log(
+//   'Всем привет! Меня зовут',
+//   myName,
+//   'Сейчас я изучаю язык программирования',
+//   programmingLanguage,
+//   'на курсе по',
+//   programmingLanguage
+// )
