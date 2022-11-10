8743b852317e46fc20c660e6658225f3a6c0d3b3
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-10 15:46:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const reasonText = 'I  am want work in IT'
 
 const numberOfMounth = '2'
 
-const allInfo = `Всем привет! Меня зовут ${myName} Сейчас я изучаю язык программирования ${programmingLanguage}  на курсе по ${programmingLanguage} У ${courseCreatorName} . Я хочу стать веб-разработчиком, потому что ${reasonText} . До этого я изучал ${programmingLanguage} ${numberOfMounth} месяца. Я уверена, что пройду данный курс до конца!"`
+const allInfo = `Всем привет! Меня зовут ${myName} Сейчас я изучаю язык программирования ${programmingLanguage}  на курсе по ${programmingLanguage} У ${courseCreatorName} . Я хочу стать веб-разработчиком, потому что ${reasonText} . До этого я изучал ${programmingLanguage} ${numberOfMounth} месяца. Я уверен, что пройду данный курс до конца!"`
 console.log(allInfo)
 
 // console.log(
